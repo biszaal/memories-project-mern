18c1de58c75a84dd1dd8ad8d9bebfc56257646cf
biszaal <biszaal@yahoo.com>
2021-09-18 15:47:31 +0545

    first commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,53 @@
+import { CREATE, UPDATE, DELETE, FETCH_ALL } from "../constants/actionTypes";
+import * as api from "../api";
+
+// Action Creators
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+
+    dispatch({ type: FETCH_ALL, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: CREATE, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
